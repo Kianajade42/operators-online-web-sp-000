9da84f77dc8b4b4e45c9a7b3e97d4c76c230f017
Kiana Long <kianajade42.kl@gmail.com>
2021-01-19 02:18:29 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,15 @@
 def unsafe?(speed)
-  if speed > 60 
-     true
-  else
-     false
-  end
+if speed <60
+  return true
+else
+  return false
 end
 
 
 
 def not_safe?(speed)
-speed < 60 ? true : false
+	speed < 60 ? true : false
 end
+	
+
+
